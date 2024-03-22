cdb051286582d6bcc49868e6108e6719b119b964
Aydin Terzi <aydinterzi7@gmail.com>
2024-03-22 20:35:56 +0300

    finish setting up next-auth

---
+++ app/header.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { ModeToggle } from "@/components/darkmode-toggle";
+import { Button } from "@/components/ui/button";
+import { signIn, signOut, useSession } from "next-auth/react";
+import React from "react";
+
+function Header() {
+  const session = useSession();
+
+  return (
+    <header>
+      <div>
+        {session.data ? (
+          <Button onClick={() => signOut()}>Sign Out</Button>
+        ) : (
+          <Button onClick={() => signIn("google")}>Sign In</Button>
+        )}
+        <ModeToggle />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ app/provider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { ThemeProvider } from "@/components/theme-provider";
+import { SessionProvider } from "next-auth/react";
+import React from "react";
+
+function Provider({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <SessionProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </SessionProvider>
+  );
+}
+
+export default Provider;
